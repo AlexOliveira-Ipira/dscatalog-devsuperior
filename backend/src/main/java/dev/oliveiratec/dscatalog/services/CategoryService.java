a0a717e307aeb33e15216f077e085e5d3c1f230d
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-11-25 10:18:54 -0300

    DTO

--- backend/src/main/java/dev/oliveiratec/dscatalog/services/CategoryService.java
+++ backend/src/main/java/dev/oliveiratec/dscatalog/services/CategoryService.java
@@ -2,11 +2,13 @@ package dev.oliveiratec.dscatalog.services;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import dev.oliveiratec.dscatalog.dto.CategoryDTO;
 import dev.oliveiratec.dscatalog.entities.Category;
 import dev.oliveiratec.dscatalog.repositories.CategoryRepository;
 
@@ -18,7 +20,8 @@ public class CategoryService implements Serializable {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
