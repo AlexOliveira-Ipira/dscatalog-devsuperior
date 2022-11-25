205953e7f318172bc2bebcfc607be86eade4e3b6
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-11-25 09:26:58 -0300

    Database seeding

--- backend/src/main/java/dev/oliveiratec/dscatalog/services/CategoryService.java
+++ backend/src/main/java/dev/oliveiratec/dscatalog/services/CategoryService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.oliveiratec.dscatalog.entities.Category;
 import dev.oliveiratec.dscatalog.repositories.CategoryRepository;
@@ -16,6 +17,7 @@ public class CategoryService implements Serializable {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
