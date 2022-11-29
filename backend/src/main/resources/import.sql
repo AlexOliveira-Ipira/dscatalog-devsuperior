e948c1599275a4142ec04b425f41ec90f3101035
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-11-29 14:19:58 -0300

    Pagination

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO TB_CATEGORY(NAME , created_At) VALUES ('Books' , NOW());
-INSERT INTO TB_CATEGORY(NAME , created_At) VALUES ('Eletronics', NOW());
+INSERT INTO TB_CATEGORY(NAME , created_At) VALUES ('Electronics', NOW());
 INSERT INTO TB_CATEGORY(NAME , created_At) VALUES ('Computers', NOW());
