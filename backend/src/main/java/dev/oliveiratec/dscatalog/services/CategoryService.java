0ec4dd1b9d95bc707856bb798db62a7d030f16f2
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-11-28 21:47:39 -0300

    Delete

--- backend/src/main/java/dev/oliveiratec/dscatalog/services/CategoryService.java
+++ backend/src/main/java/dev/oliveiratec/dscatalog/services/CategoryService.java
@@ -8,12 +8,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import dev.oliveiratec.dscatalog.dto.CategoryDTO;
 import dev.oliveiratec.dscatalog.entities.Category;
 import dev.oliveiratec.dscatalog.repositories.CategoryRepository;
+import dev.oliveiratec.dscatalog.services.exceptions.DatabaseException;
 import dev.oliveiratec.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -55,5 +58,17 @@ public class CategoryService implements Serializable {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id não encontrado" + id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id não encontrado: " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de Integridade");
+		}		
 	}
 }
