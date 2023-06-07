bc312b3bf60989cc68b841058b37c01644b25c2a
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2023-06-07 17:10:30 -0300

    Teste do Repository - Save with null id

---
+++ backend/src/test/java/dev/oliveiratec/dscatalog/factory/Factory.java
@@ -0,0 +1,20 @@
+package dev.oliveiratec.dscatalog.factory;
+
+import dev.oliveiratec.dscatalog.dto.ProductDTO;
+import dev.oliveiratec.dscatalog.entities.Category;
+import dev.oliveiratec.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+    public static Product createProduct() {
+        Product product = new Product(1L, "Phone", "Good Phonr", 800.0, "https://img.com/img.png", Instant.parse("2023-06-07T03:00:00z"));
+        product.getCategories().add(new Category(2L, "Electronics"));
+        return product;
+    }
+
+    public static ProductDTO creatProductDTO(){
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
--- backend/src/test/java/dev/oliveiratec/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/dev/oliveiratec/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package dev.oliveiratec.dscatalog.repositories;
 
 
 import dev.oliveiratec.dscatalog.entities.Product;
+import dev.oliveiratec.dscatalog.factory.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,13 +19,27 @@ public class ProductRepositoryTests {
 
    private long existingID;
    private long nonExistingId;
+   private long countTotalProducts;
 
    @BeforeEach
    void setup() {
        existingID = 1L;
        nonExistingId = 1000L;
+       countTotalProducts = 25L;
    }
 
+   @Test
+   public void saveShouldPersistWithAutoincrementWhenIdIsNull(){
+
+       Product product = Factory.createProduct();
+       product.setId(null);
+
+       product = repository.save(product);
+
+       Assertions.assertNotNull(product.getId());
+       Assertions.assertEquals(countTotalProducts + 1 , product.getId());
+
+   }
     @Test
     public void deleteShouldDeleteObjectWhenIdExists(){
         repository.deleteById(existingID);
