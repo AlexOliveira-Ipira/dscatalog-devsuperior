dd27e0668001b5c608abc99cb5cbe04b3b69aad9
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2023-06-12 16:19:46 -0300

    Teste camada web findAll

--- backend/src/test/java/dev/oliveiratec/dscatalog/resources/ProductResourceIT.java
+++ backend/src/test/java/dev/oliveiratec/dscatalog/resources/ProductResourceIT.java
@@ -8,8 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
